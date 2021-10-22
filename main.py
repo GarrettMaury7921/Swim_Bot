2a08379b8f2d0c4ce8f848db789af4a9bd7f3b12
Garrett Maury <jgmaury7921@gmail.com>
2021-10-22 18:28:51 -0400

    Basic Return the mouse coordinates after a decision
    - Prompts the user of what to do
    - Knows when the main menu is open
    - Returns position of mouse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from startup import *
 
+# Look for the main menu and then prompt the user of what the AI should do
 main_menu_starter()
 
 
--- startup.py
+++ startup.py
@@ -5,8 +5,10 @@ import keyboard
 import random
 import win32api
 import win32con
+from pynput.mouse import Button, Controller
 
 
+# Looks for Main Menu Runeterra screen
 def main_menu_starter():
     while 1:
         # MAY CHANGE IF YOU CHANGE COMPUTERS
@@ -15,8 +17,34 @@ def main_menu_starter():
                 or pyautogui.locateOnScreen('image_assets/main_menu2.png', confidence=.9) \
                 or pyautogui.locateOnScreen('image_assets/main_menu3.png', confidence=.9) is not None:
             print('I can see the main menu!')
+            _input()  # Await for input from the user
             time.sleep(0.5)
             break
         else:
             print('I cannot see it...')
             time.sleep(0.5)
+
+
+# After the main menu has been found, it asks the user for some input of what to do
+def _input():
+    loop = True
+    while loop:
+        # Should give some seconds for user to click on screen so ai doesn't get confused
+        print('Awaiting Input...')
+        print('1. Play an AI game')
+        cmdinput = input()
+        if cmdinput == '1':
+            print('Sleeping for 5, click the main menu...')
+            play_ai_game()
+            loop = False # Break the loop, a decision has been made
+        else:
+            print('Bad Input, try again')
+            continue
+
+
+# When input 1 (Play an AI game) is selected
+def play_ai_game():
+    time.sleep(5)
+    mouse = Controller()
+    current_mouse_position = mouse.position
+    print(current_mouse_position)
