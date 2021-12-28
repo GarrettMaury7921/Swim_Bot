66b7c26ce29eb540301e5f24e4a4954adcb62503
Garrett Maury <jgmaury7921@gmail.com>
2021-12-28 17:53:34 -0500

    Finding Main Menu with dimensions

--- menus.py
+++ menus.py
@@ -1,3 +1,6 @@
+import time
+
+import pyautogui
 from libraries import controls
 
 
@@ -5,6 +8,11 @@ class Menus:
 
     # Attributes
     resolution = 0
+    dimensions = ''
+    main_menu_dir = 'image_assets/'
+    # Different Displays
+    monitor1 = (1920, 1080)
+    monitor2 = (2560, 1440)
 
     def __init__(self):
         # Startup Message
@@ -14,10 +22,27 @@ class Menus:
         self.resolution = controls.get_screensize()
         print(self.get_resolution())
 
+        # Find the main menu
+        self.find_main_menu()
+
     def get_resolution(self):
-        if self.resolution == '(2560, 1440)':
-            self.resolution = 1440
-        elif self.resolution == '(1920, 1080)':
-            self.resolution = 1080
-        return 'Resolution initialized. Resolution is ' + str(self.resolution)
+        if self.resolution == self.monitor2:
+            self.dimensions = '2560x1440'
+
+        elif self.resolution == self.monitor1:
+            self.dimensions = '1920x1080'
+        return 'Resolution Initialized. Resolution is ' + self.dimensions + '.'
+
+    def find_main_menu(self):
+        # Update the main menu directory based on the dimensions of the screen
+        self.main_menu_dir = self.main_menu_dir + self.dimensions + '/main_menu/main_menu.png'
+
+        while True:
+            if pyautogui.locateOnScreen(self.main_menu_dir, confidence=.9) is not None:
+                print('Runeterra Main Menu Found.')
+                return True
+            else:
+                print('Trying to find Legends of Runeterra main menu...')
+                time.sleep(5)
+                continue
 
