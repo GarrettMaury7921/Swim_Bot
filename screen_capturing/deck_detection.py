3ce00f3d93cb67e2cc341cf96c6a50a8ef474ef3
Garrett Maury <jgmaury7921@gmail.com>
2021-12-30 18:01:47 -0500

    Better Deck finder with scrolling capability

--- screen_capturing/deck_detection.py
+++ screen_capturing/deck_detection.py
@@ -2,11 +2,13 @@ import random
 import cv2 as cv
 import pyautogui
 from time import time, sleep
+from libraries import constants1440p, constants1080p, controls
 from screen_capturing.window_capture import WindowCapture
 from screen_capturing.word_detector import WordDetector
 
 
 def detect_deck(deck, debug):
+    # Loop checkers
     loop_timeout = 0
 
     # Get deck
@@ -32,7 +34,7 @@ def detect_deck(deck, debug):
 
         word_detector.update(screenshot)
 
-        sleep(1)
+        sleep(0.07)
 
         if debug is True:
 
@@ -54,7 +56,13 @@ def detect_deck(deck, debug):
                 break
 
         loop_timeout += 1
-        if loop_timeout >= 100000:
+        if loop_timeout % 20 == 0:
+            # Go to the center of the screen and then scroll down
+            if controls.get_screensize() == (2560, 1440):
+                pyautogui.moveTo(constants1440p.CENTER_DECK_PAGE_X, constants1440p.CENTER_DECK_PAGE_Y, random.random())
+                pyautogui.scroll(-30)
+
+        if loop_timeout >= 1000:
             print('Could not find deck...')
             window_capture.stop()
             word_detector.stop()
