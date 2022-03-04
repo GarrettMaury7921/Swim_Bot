7657fcc80cd849236d6d39e823ce1ef40bb7ca83
Garrett Maury <jgmaury7921@gmail.com>
2022-03-03 19:44:06 -0500

    json loading
    - loading stats from json card files

--- bot/card_stat_finder.py
+++ bot/card_stat_finder.py
@@ -1,22 +1,8 @@
-import sys
 import json
+import sys
 
 
 def get_card_stats(all_cards):
-    # File Variables
-    set1 = sys.path[0] + "/bot/card_assets/set1-en_us.json"
-    set2 = sys.path[0] + "/bot/card_assets/set2-en_us.json"
-    set3 = sys.path[0] + "/bot/card_assets/set3-en_us.json"
-    set4 = sys.path[0] + "/bot/card_assets/set4-en_us.json"
-    set5 = sys.path[0] + "/bot/card_assets/set5-en_us.json"
-
-    # Open the files needed to search through to find the stats for each card in your hand
-    file1 = open(set1, "r")
-    file2 = open(set2, "r")
-    file3 = open(set3, "r")
-    file4 = open(set4, "r")
-    file5 = open(set5, "r")
-
     # Unpack the Card codes
     my_cards, enemy_cards, clean_codes = all_cards
 
@@ -24,10 +10,12 @@ def get_card_stats(all_cards):
     enemy_cards_clean = []
     my_cards_clean = []
     for card in clean_codes:
+
         # Check if a card is an enemy card
         for i in enemy_cards:
             if card in i:
                 enemy_cards_clean.append(card)
+
         # Check if a card is in my hand
         for i in my_cards:
             if card in i:
@@ -36,4 +24,18 @@ def get_card_stats(all_cards):
     # print(enemy_cards_clean)
     # print(my_cards_clean)
 
-    # Use json to search for codes
+    # Open the files needed to search through to find the stats for each card in your hand, encoding
+    # because riot's a bum
+    file1 = open(sys.path[0] + "/bot/card_assets/set1-en_us.json", "r", encoding='utf-8')
+    file2 = open(sys.path[0] + "/bot/card_assets/set2-en_us.json", "r", encoding='utf-8')
+    file3 = open(sys.path[0] + "/bot/card_assets/set3-en_us.json", "r", encoding='utf-8')
+    file4 = open(sys.path[0] + "/bot/card_assets/set4-en_us.json", "r", encoding='utf-8')
+    file5 = open(sys.path[0] + "/bot/card_assets/set5-en_us.json", "r", encoding='utf-8')
+
+    # Search for the codes in the card dictionary
+    set1 = json.load(file1)
+    set2 = json.load(file2)
+    set3 = json.load(file3)
+    set4 = json.load(file4)
+    set5 = json.load(file5)
+
