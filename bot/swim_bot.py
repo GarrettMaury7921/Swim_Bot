cec6c78583966c1b6941b9261f62d4734dd5cf0a
Garrett Maury <jgmaury7921@gmail.com>
2022-03-12 12:24:40 -0500

    get_card_stats method in swim_bot.py

--- bot/swim_bot.py
+++ bot/swim_bot.py
@@ -11,18 +11,22 @@ class Bot:
 
     def __init__(self, debug):
         # Initializes all classes that run with the bot, finds cards and card stats
-        bot_initializer = Initializer(debug)
+        self.bot_initializer = Initializer(debug)
         # Stats the thread in the initializer that updates the stats that the card finder finds
-        bot_initializer.start()
+        self.bot_initializer.start()
 
         time.sleep(3)
 
+        self.get_card_stats()
+
+    def get_card_stats(self):
         while True:
 
             # all_stats = [my_cards_stats, enemy_cards_stats]
             # all_cards = my_cards, enemy_cards, clean_codes - has x and y values
-            all_stats = bot_initializer.all_stats
-            all_cards = bot_initializer.all_cards
+            all_stats = self.bot_initializer.all_stats
+            all_cards = self.bot_initializer.all_cards
 
             print(all_stats)
             print(all_cards)
+
