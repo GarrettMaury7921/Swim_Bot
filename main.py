768accd4793d0fad93c3ec206df061c0010e80fa
Garrett Maury <jgmaury7921@gmail.com>
2022-03-26 13:04:06 -0400

    Turning on the whole program for show

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import time
-
-from libraries.controls import get_cursor_position
 from menus import Menus
 from screen_capturing import deck_detection
 from bot.swim_bot import Bot
@@ -9,13 +6,11 @@ from bot.swim_bot import Bot
 
 DEBUGGING = True
 
-# time.sleep(3)
-# print(get_cursor_position())
 # Initialize the resolution and ask the user for an action for the bot to take
-# initialize_menu = Menus()
-# if initialize_menu.action == '1':
-#     # Input in the selected deck, Have True set for debugging (OpenCV Window)
-#     deck_detection.detect_deck(initialize_menu.deck, DEBUGGING)
+initialize_menu = Menus()
+if initialize_menu.action == '1':
+    # Input in the selected deck, Have True set for debugging (OpenCV Window)
+    deck_detection.detect_deck(initialize_menu.deck, DEBUGGING)
 
 # Put the stats in the swim_bot and initialize the bot script
 swim_bot = Bot(DEBUGGING)
